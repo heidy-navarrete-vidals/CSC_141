62288547ddec6c2dba0ec7ee61bada475ba150e6
heidyN04 <heidynavarrete001@gmail.com>
2024-08-29 10:07:41 -0400

    First Commit

---
+++ 01_getting_started/01_02_hello_world_typos.py
@@ -0,0 +1,5 @@
+# this will contain a typo
+
+("Hello World!")
+
+''' this code will not work because it does not have the print command before the text'''
---
+++ 01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,5 @@
+# I would love to make a pixel art video game
+
+# A 3D horror game about the ocean
+
+# A website containing all of my project and profile information
